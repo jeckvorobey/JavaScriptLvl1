6f350a0b059f9915182d73cec6b18cdebc5af71e
Jeckvorobey <jeckvorobey69@gmail.com>
2018-12-10 22:47:08 +0300

    Доделал решение первого задания

--- script/6.js
+++ script/6.js
@@ -8,13 +8,18 @@ function init() {
 
 function changeBigPicture(eventObj) {
 
-    var appDiv = document.getElementsByClassName("bigImg");
+    var appDiv = document.getElementById("bigImg");
     appDiv.innerHTML = "";
     var eventElement = eventObj.target;
     var imageNameParts = eventElement.id;
     var src = "./img/bigPic/" + imageNameParts + ".jpg";
     var imageDomElement = document.createElement("img");
     imageDomElement.src = src;
+    imageDomElement.style.width = "640px";
+    imageDomElement.style.height = "480px";
+    imageDomElement.onerror = function init() {
+        alert("Изображение не доступно")
+    };
     appDiv.appendChild(imageDomElement);
 }
 window.onload = init;
